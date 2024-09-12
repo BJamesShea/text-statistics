a95e59c7acfb7baff836cbc73f5ac0a793f0fef0
Brandon Shea <brandon.shea1994@icloud.com>
2024-09-12 09:48:05 -0230

    Add character counting dquote> - Add support for counting the characters in the provided string

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const process = require("node:process");
 function getWordCount(text) {
   return text.trim().split(" ").length;
 }
+
+function getCharacterCount(text) {
+  return text.length;
+}
 function printHelpMessage() {
   console.log(`
     Usage: text-statistics "<text>"
@@ -12,7 +16,7 @@ function printHelpMessage() {
     text-statistics "Hello World. This is a test"
 
     Output:
-    The number of words in "Hello world. This is a test" is: 6
+    The number of words in "Hello world. This is a test" is: 6 and the number of characters is: 28
     
     `);
 }
@@ -30,5 +34,5 @@ if (userArguments[0] === "--help" || userArguments[0] === "-h") {
 console.log(
   `The number of words in "${userArguments[0]}" is: ${getWordCount(
     userArguments[0]
-  )}`
+  )} and the number of characters is: ${getCharacterCount(userArguments[0])}`
 );
