245dbe32bc44befb5366a662ff7b854ff7014dff
Brandon Shea <brandon.shea1994@icloud.com>
2024-09-12 09:34:32 -0230

    Init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const process = require("node:process");
+
+function getWordCount(text) {
+  return text.trim().split(" ").length;
+}
+function printHelpMessage() {
+  console.log(`
+    Usage: text-statistics "<text>"
+    Example:
+    text-statistics "Hello World. This is a test"
+
+    Output:
+    The number of words in "Hello world. This is a test" is: 6
+    
+    `);
+}
+const userArguments = process.argv.slice(2);
+if (userArguments.length !== 1) {
+  printHelpMessage();
+  return;
+}
+
+if (userArguments[0] === "--help" || userArguments[0] === "-h") {
+  printHelpMessage();
+  return;
+}
+
+console.log(
+  `The number of words in "${userArguments[0]}" is: ${getWordCount(
+    userArguments[0]
+  )}`
+);
